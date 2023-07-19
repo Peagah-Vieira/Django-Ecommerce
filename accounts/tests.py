a2ae7ea90c1599a6a9b1953559ed71f69c19071a
Peagah <peagahvieira2003@gmail.com>
2023-07-19 02:02:16 -0300

    test: Accounts - Endpoint (me)

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,51 @@
-from django.test import TestCase
+from rest_framework import test
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your tests here.
+
+class AccountsAPITest(test.APITestCase):
+    def setUp(self, *args, **kwargs):
+        self.user_data = {
+            'username': 'admin',
+            'password': 'admin',
+        }
+        return super().setUp(*args, **kwargs)
+
+    def make_user(self):
+        User.objects.create_user(
+            username=self.user_data['username'],
+            password=self.user_data['password'],
+        )
+
+    def get_jwt_refresh_token(self):
+        self.make_user()
+        response = self.client.post(
+            reverse('token_obtain_pair'),
+            data={**self.user_data}
+        )
+
+        return response.data.get('refresh')
+
+    def get_jwt_access_token(self):
+        self.make_user()
+        response = self.client.post(
+            reverse('token_obtain_pair'),
+            data={**self.user_data}
+        )
+
+        return response.data.get('access')
+
+    def test_unauthorized_user_cant_see_informations(self):
+        # 401 = Unauthorized
+        me_url = reverse('accounts:accounts-api-me')
+        response = self.client.get(me_url)
+        self.assertEqual(response.status_code, 401)
+
+    def test_authorized_user_can_see_informations(self):
+        # 200 = Ok
+        me_url = reverse('accounts:accounts-api-me')
+        response = self.client.get(
+            me_url,
+            HTTP_AUTHORIZATION=f'Bearer {self.get_jwt_access_token()}',
+        )
+        self.assertEqual(response.status_code, 200)
