2c8e9314a08e21f26879342e312f312fbb560c69
Peagah <peagahvieira2003@gmail.com>
2023-07-19 06:18:05 -0300

    feat: Products - Model create

--- products/models.py
+++ products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=25, unique=True)
+    description = models.TextField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=50, unique=True)
+    description = models.TextField(max_length=100)
+    category = models.ForeignKey(
+        Category,
+        related_name='product_category',
+        on_delete=models.CASCADE
+    )
+    price = models.DecimalField(decimal_places=2, max_digits=10)
