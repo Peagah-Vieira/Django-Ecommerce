6d8376a7ef7e666f4a7f33414edffe358534af4e
Peagah <peagahvieira2003@gmail.com>
2023-07-19 06:47:20 -0300

    feat: Products - All CRUD endpoints create

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,16 @@ from products import views
 
 app_name = "products"
 
+categories_api_router = SimpleRouter(trailing_slash=True)
+categories_api_router.register(
+    prefix='categories/api',
+    viewset=views.CategoriesAPIViewSet,
+    basename='categories-api'
+)
+
+urlpatterns = categories_api_router.urls
+
+
 products_api_router = SimpleRouter(trailing_slash=True)
 products_api_router.register(
     prefix='api',
@@ -10,4 +20,4 @@ products_api_router.register(
     basename='products-api'
 )
 
-urlpatterns = products_api_router.urls
+urlpatterns += products_api_router.urls
--- products/views.py
+++ products/views.py
@@ -1,5 +1,15 @@
 from rest_framework.viewsets import ModelViewSet
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
+
+
+class CategoriesAPIViewSet(ModelViewSet):
+    http_method_names = ['get', 'post', 'patch', 'delete']
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
 
 
 class ProductsAPIViewSet(ModelViewSet):
-    ...
+    http_method_names = ['get', 'post', 'patch', 'delete']
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
