84c60b9892d00a35b63979d0ecad09545920242f
Peagah <peagahvieira2003@gmail.com>
2023-07-20 08:01:20 -0300

    feat: Products - Nested serializer for more informations

--- products/models.py
+++ products/models.py
@@ -7,15 +7,21 @@ class Category(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     title = models.CharField(max_length=50, unique=True)
     description = models.TextField(max_length=100)
     category = models.ForeignKey(
         Category,
-        related_name='product_category',
+        related_name='category',
         on_delete=models.CASCADE
     )
     price = models.DecimalField(decimal_places=2, max_digits=10)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
--- products/serializers.py
+++ products/serializers.py
@@ -26,3 +26,21 @@ class ProductSerializer(serializers.ModelSerializer):
             'created_at',
             'updated_at',
         ]
+
+    category = CategorySerializer()
+
+    def create(self, validated_data):
+        categories_data = validated_data.pop("category")
+        category = Category.objects.create(**categories_data)
+        product = Product.objects.create(category=category, **validated_data)
+        return product
+
+    def update(self, instance, validated_data):
+        if 'category' in validated_data:
+            nested_serializer = self.fields['category']
+            nested_instance = instance.category
+            nested_data = validated_data.pop('category')
+
+            nested_serializer.update(nested_instance, nested_data)
+
+        return super(ProductSerializer, self).update(instance, validated_data)
