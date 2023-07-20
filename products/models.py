c54b646dd4761746dc13ae1fe791298627abe840
Peagah <peagahvieira2003@gmail.com>
2023-07-20 10:00:18 -0300

    refactor: Products - Models

--- products/models.py
+++ products/models.py
@@ -2,20 +2,41 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=25, unique=True)
-    description = models.TextField(max_length=50)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(
+        max_length=25,
+        unique=True,
+    )
+    description = models.TextField(
+        max_length=50,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+    )
 
 
 class Product(models.Model):
-    title = models.CharField(max_length=50, unique=True)
-    description = models.TextField(max_length=100)
+    title = models.CharField(
+        max_length=50,
+        unique=True,
+    )
+    description = models.TextField(
+        max_length=100,
+    )
     category = models.ForeignKey(
         Category,
-        related_name='product_category',
-        on_delete=models.CASCADE
+        related_name='category',
+        on_delete=models.CASCADE,
     )
-    price = models.DecimalField(decimal_places=2, max_digits=10)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    price = models.DecimalField(
+        decimal_places=2,
+        max_digits=10,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+    )
