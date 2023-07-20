f8ef1f312b4c3ce2050c585840c1ef68caea677e
Peagah <peagahvieira2003@gmail.com>
2023-07-20 10:45:38 -0300

    feat: Orders - Details implemented

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from accounts.serializers import AccountsSerializer
+from products.serializers import ProductSerializer
 from .models import Order, OrderItem
 
 
@@ -8,12 +10,18 @@ class OrderSerializer(serializers.ModelSerializer):
         fields = [
             'id',
             'buyer',
+            'buyer_details',
             'order_number',
             'status',
             'is_paid',
             'created_at',
             'updated_at',
         ]
+
+    buyer_details = AccountsSerializer(
+        source='buyer',
+        read_only=True,
+    )
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
@@ -22,9 +30,21 @@ class OrderItemSerializer(serializers.ModelSerializer):
         fields = [
             'id',
             'order',
+            'order_details',
             'product',
+            'product_details',
             'quantity',
             'total',
             'created_at',
             'updated_at',
         ]
+
+    order_details = OrderSerializer(
+        source='order',
+        read_only=True,
+    )
+
+    product_details = ProductSerializer(
+        source='product',
+        read_only=True,
+    )
